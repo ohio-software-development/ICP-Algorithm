3b68e643708ffdf7cbbecc599395125fd281947a
Roselyn <chenkatti@tutanota.com>
2025-02-10 03:15:36 -0500

    Adding new point cloud files

--- icp-algorithm-open3d.py
+++ icp-algorithm-open3d.py
@@ -1,6 +1,17 @@
 import open3d as o3d
 import copy
-import numpy as np
+
+
+def draw_registration(source_file):
+    source = o3d.io.read_point_cloud(source_file)
+    o3d.visualization.draw_geometries(
+        [source],
+        window_name="Linux Supremacy, Linux Supremacy",
+        zoom=0.4459,
+        front=[0.9288, -0.2951, -0.2242],
+        lookat=[1.6784, 2.0612, 1.4451],
+        up=[-0.3402, -0.9189, -0.1996],
+    )
 
 
 def draw_registration_result(source, target, transformation):
@@ -11,7 +22,7 @@ def draw_registration_result(source, target, transformation):
     source_temp.transform(transformation)
     o3d.visualization.draw_geometries(
         [source_temp, target_temp],
-        window_name="Open3D Window",
+        window_name="Linux Supremacy, Linux Supremacy",
         zoom=0.4459,
         front=[0.9288, -0.2951, -0.2242],
         lookat=[1.6784, 2.0612, 1.4451],
@@ -19,40 +30,76 @@ def draw_registration_result(source, target, transformation):
     )
 
 
-demo_icp_pcds = o3d.data.DemoICPPointClouds()
-source = o3d.io.read_point_cloud(demo_icp_pcds.paths[0])
-target = o3d.io.read_point_cloud(demo_icp_pcds.paths[1])
-source.estimate_normals()
-target.estimate_normals()
+def global_registration(source, target):
+    print("Finding global registration...")
+    radius_normal = 0.05
+    radius_feature = 0.1
 
-threshold = 0.02
-trans_init = np.asarray(
-    [
-        [0.862, 0.011, -0.507, 0.5],
-        [-0.139, 0.967, -0.215, 0.7],
-        [0.487, 0.255, 0.835, -1.4],
-        [0.0, 0.0, 0.0, 1.0],
-    ]
-)
-draw_registration_result(source, target, trans_init)
+    source.estimate_normals(
+        search_param=o3d.geometry.KDTreeSearchParamHybrid(
+            radius=radius_normal, max_nn=30
+        )
+    )
+    target.estimate_normals(
+        search_param=o3d.geometry.KDTreeSearchParamHybrid(
+            radius=radius_normal, max_nn=30
+        )
+    )
 
-print("Initial alignment")
-evaluation = o3d.pipelines.registration.evaluate_registration(
-    source, target, threshold, trans_init
-)
-print(evaluation)
+    source_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
+        source, o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100)
+    )
+    target_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
+        target, o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100)
+    )
+
+    # Global Registration (Fast Global Registration)
+    distance_threshold = 0.1
+    result = o3d.pipelines.registration.registration_fgr_based_on_feature_matching(
+        source,
+        target,
+        source_fpfh,
+        target_fpfh,
+        o3d.pipelines.registration.FastGlobalRegistrationOption(
+            maximum_correspondence_distance=distance_threshold
+        ),
+    )
+    print("Finished calculating global registration")
+    print(result)
+    return result
 
 
-print("Apply point-to-plane ICP")
-reg_p2l = o3d.pipelines.registration.registration_icp(
-    source,
-    target,
-    threshold,
-    trans_init,
-    o3d.pipelines.registration.TransformationEstimationPointToPlane(),
-    o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=2000),
-)
-print(reg_p2l)
-print("Transformation is:")
-print(reg_p2l.transformation)
-draw_registration_result(source, target, reg_p2l.transformation)
+def main():
+    demo_icp_pcds = o3d.data.DemoICPPointClouds()
+    source = o3d.io.read_point_cloud(demo_icp_pcds.paths[0])
+    target = o3d.io.read_point_cloud(demo_icp_pcds.paths[1])
+
+    result = global_registration(source, target)
+
+    threshold = 0.02
+    draw_registration_result(source, target, result.transformation)
+
+    print("Initial alignment")
+    evaluation = o3d.pipelines.registration.evaluate_registration(
+        source, target, threshold, result.transformation
+    )
+    print(evaluation)
+
+    print("Apply point-to-plane ICP")
+    reg_p2l = o3d.pipelines.registration.registration_icp(
+        source,
+        target,
+        threshold,
+        result.transformation,
+        o3d.pipelines.registration.TransformationEstimationPointToPlane(),
+        o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=2000),
+    )
+    print(reg_p2l)
+    print("Transformation is:")
+    print(reg_p2l.transformation)
+    draw_registration_result(source, target, reg_p2l.transformation)
+
+
+if __name__ == "__main__":
+    main()
+    # draw_registration("./data/ism_test_horse.pcd")
